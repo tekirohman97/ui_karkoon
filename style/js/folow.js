6b66371924869dbfd403cd80a9bbc93c5a5bd09c
tekirohman97 <tekkirohman@gmailcom>
2021-07-28 15:41:46 +0700

    menambah page folowing dan folower

---
+++ style/js/folow.js
@@ -0,0 +1,17 @@
+let tabHeader = document.getElementsByClassName("tabbar-header")[0];
+let tabIndicator = document.getElementsByClassName("tabbar-indicator")[0];
+let tabBody = document.getElementsByClassName("tabbar-body")[0];
+
+
+let tabsPane = tabHeader.getElementsByTagName("div");
+
+for (let i = 0; i < tabsPane.length; i++) {
+    tabsPane[i].addEventListener("click", function () {
+        tabHeader.getElementsByClassName("active")[0].classList.remove("active");
+        tabsPane[i].classList.add("active");
+        tabBody.getElementsByClassName("active")[0].classList.remove("active");
+        tabBody.getElementsByTagName("div")[i].classList.add("active");
+
+        tabIndicator.style.left = `calc(calc(100% / 2) * ${i})`;
+    });
+}
